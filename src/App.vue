7dba2d3eb6f35079867bf3a0cc6b6e1276b24b46
Vadimus <vesvadvlad1@gmail.com>
2023-04-08 12:21:18 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app">
+    <div class="app__content-wrapper">
+      <Object1 style="position: absolute; left: 40%; top: 20%" />
+      <Object1 style="position: absolute; left: 50%; top: 50%" />
+      <Object1 style="position: absolute; right: 40%; bottom: 15%" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Object1 from "./components/objects/Object1.vue";
+
+export default {
+  components: { Object1 },
+};
+</script>
+
+<style lang="scss">
+.app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  &__content-wrapper {
+    position: relative;
+    flex-grow: 1;
+  }
+}
+</style>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/objects/Object1.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    @animationend="deactivate"
+    class="object"
+    :class="{
+      object_activated: isActivated,
+    }"
+  >
+    <audio controls preload ref="audio">
+      <source src="@/assets/sounds/1.mp3" type="audio/mp3" />
+    </audio>
+    <div class="object__image-wrapper">
+      <img class="object__image" src="@/assets/images/1.jpg" />
+    </div>
+    <div @click="activate">
+      <div
+        style="
+          position: absolute;
+          transform: translate(-40%, -50%);
+          width: 232px;
+          height: 165px;
+          border-radius: 50%;
+          cursor: pointer;
+          /*background: rgba(255, 0, 0, 15%);*/
+        "
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["activate"],
+
+  data() {
+    return {
+      isActivated: false,
+    };
+  },
+
+  methods: {
+    activate() {
+      this.$refs.audio.play();
+      this.isActivated = true;
+    },
+
+    deactivate() {
+      this.isActivated = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@keyframes object1 {
+  from {
+    transform: none;
+  }
+  5% {
+    transform: rotate(6deg) translateY(-15px);
+  }
+  15% {
+    transform: scale(1.05) rotate(-7deg) translateY(-25px);
+  }
+  25% {
+    transform: scale(1.07) rotate(2deg) translateY(-50px);
+  }
+  to {
+    transform: none;
+  }
+}
+
+.object {
+  width: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 0;
+  &_activated {
+    animation: object1 2.5s ease 0s 3 normal;
+  }
+
+  &__image {
+    width: 300px;
+    height: auto;
+    pointer-events: none;
+  }
+
+  &__image-wrapper {
+    width: 0;
+    height: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
